0262fccc339676efb88191c5ca5a8583e2cd4862
Kristoffertolboll <yogistrikez@gmail.com>
2018-04-25 16:42:23 +0200

    new commit

--- src/main/java/PresentationLayer/Register.java
+++ src/main/java/PresentationLayer/Register.java
@@ -13,32 +13,45 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class Register extends Command {
+
     PasswordEncryption PE = new PasswordEncryption();
 
     @Override
-    String execute( HttpServletRequest request, HttpServletResponse response ) throws LoginSampleException {
-        String email = request.getParameter( "email" );
+    String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
+        String email = request.getParameter("email");
         String username = request.getParameter("username");
         String phonenumber = request.getParameter("phonenumber");
-        
-        String password1 = request.getParameter( "password1" );
-        String password2 = request.getParameter( "password2" );
-        if ( password1.equals( password2 ) ) {
+
+        String password1 = request.getParameter("password1");
+        String password2 = request.getParameter("password2");
+        if (password1.equals(password2)) {
+
+            byte[] salt = null;
             try {
-                byte[] salt = PE.genereteSalt();
-              byte[] passoword= PE.getEncryptedPassword(password1, salt);
+                salt = PE.genereteSalt();
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            byte[] password = null;
+            try {
+                password = PE.getEncryptedPassword(password1, salt);
             } catch (NoSuchAlgorithmException ex) {
                 Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
             } catch (InvalidKeySpecException ex) {
                 Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
             }
-            User user = LogicFacade.createUser(0, username, email, password1, email);
+            User user = null;
+            try {
+                user = LogicFacade.createUser(0, username, email, password, email);
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+            }
             HttpSession session = request.getSession();
-            session.setAttribute( "user", user );
-            session.setAttribute( "role", user.getRole() );
+            session.setAttribute("user", user);
+            session.setAttribute("role", user.getRole());
             return user.getRole() + "page";
         } else {
-            throw new LoginSampleException( "the two passwords did not match" );
+            throw new LoginSampleException("the two passwords did not match");
         }
     }
 
