f7e8ee9a4fd885bc02f35ba9b66423a206704e90
Kristoffertolboll <yogistrikez@gmail.com>
2018-04-26 18:12:36 +0200

    test commit

--- src/main/java/PresentationLayer/Login.java
+++ src/main/java/PresentationLayer/Login.java
@@ -21,20 +21,15 @@ public class Login extends Command {
 
     @Override
     String execute( HttpServletRequest request, HttpServletResponse response ) throws LoginSampleException {
-        try {
-            PasswordEncryption PE = new PasswordEncryption();
-            
-            String email = request.getParameter( "email" );
-            String password = request.getParameter( "password" );
-            User user = LogicFacade.login(email, password);
-            HttpSession session = request.getSession();
-            session.setAttribute( "user", user );
-            session.setAttribute( "role", user.getRole() );
-            return user.getRole() + "page";
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
+        PasswordEncryption PE = new PasswordEncryption();
+        
+        String email = request.getParameter( "email" );
+        String password = request.getParameter( "password" );
+        User user = LogicFacade.login(email, password);
+        HttpSession session = request.getSession();
+        session.setAttribute( "user", user );
+        session.setAttribute( "role", user.getRole() );
+        return user.getRole() + "page";
     }
 
 }
