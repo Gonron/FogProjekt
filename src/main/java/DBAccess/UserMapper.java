681e11a81639e4866888b7fb2a1d9a2fbafb7743
MortenBB <mortenbb1@gmail.com>
2018-05-01 14:31:54 +0200

    lavet update order og getorders(user)

--- src/main/java/DBAccess/UserMapper.java
+++ src/main/java/DBAccess/UserMapper.java
@@ -12,64 +12,59 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-
 /**
- The purpose of UserMapper is to...
-
- @author kasper
+ * The purpose of UserMapper is to...
+ *
+ * @author kasper
  */
 public class UserMapper {
-    
-   
 
-    
-    public static void createUser( User user ) throws LoginSampleException {
+    public static void createUser(User user) throws LoginSampleException {
         try {
             Connection con = Connector.connection();
             String SQL = "INSERT INTO Users (email, password, role) VALUES (?, ?, ?)";
-            PreparedStatement ps = con.prepareStatement( SQL, Statement.RETURN_GENERATED_KEYS );
-            ps.setString( 1, user.getEmail() );
-            ps.setBytes( 2, user.getPassword() );
-            ps.setString( 3, user.getRole() );
+            PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getEmail());
+            ps.setBytes(2, user.getPassword());
+            ps.setString(3, user.getRole());
             ps.executeUpdate();
             ResultSet ids = ps.getGeneratedKeys();
             ids.next();
-            int id = ids.getInt( 1 );
-            user.setId( id );
-        } catch ( SQLException | ClassNotFoundException ex ) {
-            throw new LoginSampleException( ex.getMessage() );
+            int id = ids.getInt(1);
+            user.setId(id);
+        } catch (SQLException | ClassNotFoundException ex) {
+            throw new LoginSampleException(ex.getMessage());
         }
     }
 
+    public static User login(String email, String password) throws LoginSampleException, NoSuchAlgorithmException {
 
-    public static User login( String email, String password ) throws LoginSampleException, NoSuchAlgorithmException {
-
-        
         // denne metode skal rettes så ledes, at vi tager et salt objekt udfra databasen og kan bruge det til at verificere brugeren
         try {
             PasswordEncryption PE = new PasswordEncryption();
             Connection con = Connector.connection();
             String SQL = "SELECT id, role FROM Users "
                     + "WHERE email=? AND password=?";
-            PreparedStatement ps = con.prepareStatement( SQL );
-            ps.setString( 1, email );
-            ps.setBytes(2, password );
-            ps.setBytes( 2, password );
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setString(1, email);
+            ps.setBytes(2, password);
+            ps.setBytes(2, password);
             ResultSet rs = ps.executeQuery();
-            if ( rs.next() ) {
-                String role = rs.getString( "role" );
-                int id = rs.getInt( "id" );
+            if (rs.next()) {
+                String role = rs.getString("role");
+                int id = rs.getInt("id");
                 User user = new User(role, role, email, password, role, salt);
-                user.setId( id );
+                user.setId(id);
                 return user;
             } else {
-                throw new LoginSampleException( "Could not validate user" );
+                throw new LoginSampleException("Could not validate user");
             }
-        } catch ( ClassNotFoundException | SQLException ex ) {
+        } catch (ClassNotFoundException | SQLException ex) {
             throw new LoginSampleException(ex.getMessage());
         }
     }
- public static ArrayList<Order> getOrders() throws ClassNotFoundException, SQLException {
+
+    public static ArrayList<Order> getOrders() throws ClassNotFoundException, SQLException {
         ArrayList<Order> orders = new ArrayList();
         Connection con = Connector.connection();
         String SQL = "SELECT * FROM orders";
@@ -86,8 +81,8 @@ public class UserMapper {
         }
         return orders;
     }
- 
-  public static void createOrder(Order order, User user) throws SQLException, ClassNotFoundException {
+
+    public static void createOrder(Order order, User user) throws SQLException, ClassNotFoundException {
 
         Connection con = Connector.connection();
         String SQL = "INSERT INTO orders (id, Heigth, Width, Length, status) VALUES (?, ?, ?, ?, ?)";
@@ -104,11 +99,32 @@ public class UserMapper {
         order.setId(id);
     }
 
-    public static ArrayList<Order> getOrders(User u) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static ArrayList<Order> getOrders(User u) throws ClassNotFoundException, SQLException {
+        int id = u.getId();
+        ArrayList<Order> orders = new ArrayList();
+        Connection con = Connector.connection();
+        String SQL = "SELECT * FROM orders " + "WHERE id=?";
+        PreparedStatement ps = con.prepareStatement(SQL);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            int orderId = rs.getInt("order_id");
+            int heigth = rs.getInt("Heigth");
+            int width = rs.getInt("Width");
+            int length = rs.getInt("Length");
+            boolean status = rs.getBoolean("status");
+            Order order = new Order(orderId, heigth, width, length, status);
+            orders.add(order);
+        }
+        return orders;
     }
 
-    public static void updateOrder(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public static void updateOrder(int id) throws ClassNotFoundException, SQLException {
+        Connection con = Connector.connection();
+        String SQL = "update orders set status = ? where order_id = ?";
+        PreparedStatement ps = con.prepareStatement(SQL);
+        ps.setBoolean(1, true);
+        ps.setInt(2, id);
+        ps.executeUpdate();
     }
 }
