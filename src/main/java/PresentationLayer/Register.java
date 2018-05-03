92e8cfba7d98150c0106841c33ba1a62574e8443
Kristoffer Tolboll <yogistrikez@gmail.com>
2018-05-03 12:31:33 +0200

    håber ikke på merge konflikter

--- src/main/java/PresentationLayer/Register.java
+++ src/main/java/PresentationLayer/Register.java
@@ -19,31 +19,29 @@ public class Register extends Command {
     @Override   
     String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
         String email = request.getParameter("email");
+        String username = request.getParameter("username");
         String phonenumber = request.getParameter("phonenumber");
         int postalCode = Integer.parseInt(request.getParameter("postalcode"));
         String address = request.getParameter("address");
+
         String password1 = request.getParameter("password1");
         String password2 = request.getParameter("password2");
         if (password1.equals(password2)) {
 
-//            byte[] salt = null;
-//            try {
-//                salt = PE.genereteSalt();
-//            } catch (NoSuchAlgorithmException ex) {
-//                Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//            byte[] password = null;
-//            try {
-//                password = PE.getEncryptedPassword(password1, salt);
-//            } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
-//                Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
-//            }
+            byte[] salt = null;
+            try {
+                salt = PE.genereteSalt();
+            } catch (NoSuchAlgorithmException ex) {
+                Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            byte[] password = null;
+            try {
+                password = PE.getEncryptedPassword(password1, salt);
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+                Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
+            }
             User user = LogicFacade.createUser(phonenumber, email, password2, postalCode, address);
-//            try {
-//                user = LogicFacade.createUser(username, phonenumber, email, password2, postalCode, address);
-//            } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
-//                Logger.getLogger(Register.class.getName()).log(Level.SEVERE, null, ex);
-//            }
+            
             HttpSession session = request.getSession();
             session.setAttribute("user", user);
             session.setAttribute("role", user.getRole());
